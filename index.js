bc9e536655f30cd124e3b6ad6d7abd50f909a158
Yaswanth2k4 <kurapati.yeswanth64@gmail.com>
2023-10-10 21:27:29 +0530

    Modified index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import parser from "body-parser";
 import dotenv from "dotenv";
 import {LoginRoute} from "./routes/LoginRoute.js";
-import { NotesRoute } from "./routes/NotesRoute.js";
+import { NotesRoute } from "./routes/NotesRoute.js";z
 
 const app=express();
 app.use(parser.urlencoded({extended:true}));
