2810dd69a31645b9616ffc125dea18bfa2b8a739
Yaswanth2k4 <118299901+Yaswanth2k4@users.noreply.github.com>
2023-10-10 14:55:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,26 +3,15 @@ import mongoose from "mongoose";
 import parser from "body-parser";
 import dotenv from "dotenv";
 import {LoginRoute} from "./routes/LoginRoute.js";
-import { NotesRoute } from "./routes/NotesRoute.js";
-import path from "path";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
+import { NotesRoute } from "./routes/NotesRoute.js";z
 
 const app=express();
 app.use(parser.urlencoded({extended:true}));
 app.use(parser.json());
-app.use(express.static(path.join(__dirname,"public")));
 
 dotenv.config();
 
 mongoose.connect(process.env.DATABASE);
-
-app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"public","index.html"));
-})
 
 app.use("/",LoginRoute);
 app.use("/notes",NotesRoute);
